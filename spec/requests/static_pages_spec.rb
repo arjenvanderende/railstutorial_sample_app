bbccd76d1bd4563fc4f82a9435cb7333ec425f26
Arjen van der Ende <arjen.vanderende@gmail.com>
2014-01-04 14:50:37 +0100

    Add integration tests for home and help page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit static_pages_home_path
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit static_pages_help_path
+      expect(page).to have_content('Help')
+    end
+  end
+end
