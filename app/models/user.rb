d08eebf2bbf7f9bb1d450112d5bc914c41dd5222
Arjen van der Ende <arjen.vanderende@gmail.com>
2014-01-08 00:35:50 +0100

    Add password digest to user model with validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  has_secure_password
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
