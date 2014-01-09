a294eb0dbcef1d87bd6073686fc37b4792a4b17c
Arjen van der Ende <arjen.vanderende@gmail.com>
2014-01-09 19:46:22 +0100

    Create user on successful sign up and redirect to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    render 'new'
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   private
